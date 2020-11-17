bd9dc4470224b5fb7607701887374aafc6a4b29b
Honore Niyitegeka <niyitegekah@gmail.com>
2020-11-17 19:33:20 +0200

    ft(structure): implement layout structure - add footer section - meta icons - folder structure

---
+++ components/Footer.js
@@ -0,0 +1,31 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
+export default function Footer() {
+    return(
+        <footer className='footer-section w-100 py-5'>
+            <Container className='mt-5 mb-5 py-5'>
+                <Row>
+                    <Col lg={6} md={6} sm={12} xs={12}>
+                        <Row>
+                            <div className='content'>
+                                <h2>
+                                    I'm always looking forward to new opportunities, Feel free to say hello 👋 via <a href='mailto:niyitegekah@gmail.com'>niyitegekah@gmail.com</a>
+                                </h2>
+                            </div>
+                        </Row>
+
+                        <Row className='mt-5'>
+                            <div className='social'>
+                                <a href='https://www.linkedin.com/in/nythonore/' target="_blank">Linkedin</a>
+                                <a href='https://twitter.com/nythonore' target="_blank">Twitter</a>
+                                <a href='https://www.instagram.com/nythonore/' target="_blank">Instagram</a>
+                                <a href='https://github.com/nythonore' target="_blank">Github</a>
+                                <a href='https://codepen.io/nythonore' target="_blank">Codepen</a>
+                            </div>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
